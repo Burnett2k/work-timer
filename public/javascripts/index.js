27a3639d034075b78d1ea34bfa906da758f5b253
Sawyer B <sawyerburnett@gmail.com>
2018-03-17 09:08:35 -0700

    fixes issue #17

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,25 +6,43 @@
 	var tick = document.getElementById("tick");
 	var timesUp = document.getElementById("timesUp");
 	var mute = true;
+	var timerRunning = false;
+	var secondsRemaining;
 
+
+	/*
+		BUTTON EVENTS
+	*/
  	$('#start').click(function() {
 
- 		if (interval) {
+ 		if (timerRunning) {
  			clearInterval(interval);
+ 			timerRunning = false;
+ 		} else {
+ 			var duration;
+
+ 			if (secondsRemaining > 0) {
+ 				duration = secondsRemaining;
+ 			} else {
+				duration = $('#minutes').val() * 60;
+ 			}
+
+		 	startTimer(duration, display);
+		 	timerRunning = true;
  		}
 
-	 	var duration = $('#minutes').val() * 60;
-
-	 	startTimer(duration, display);
+ 		updatePlayButtontext();
  	});
 
  	$('#stop').click(function() {
  		stopTimer();
+ 		updatePlayButtontext();
  	});
 
  	$('#reset').click(function() {
 	 	var duration = $('#minutes').val() * 60;
  		resetTimer(duration, display);
+ 		updatePlayButtontext();
  	});
 
  	$('#save').click(function() {
@@ -39,8 +57,10 @@
  		var minutes;
  		var seconds;
 
+
  		function timer() {
  			diff = duration - (((Date.now() - start) / 1000) | 0);
+ 			secondsRemaining = diff;
  			minutes = (diff / 60) | 0;
  			seconds = (diff % 60) | 0;
  			minutes = minutes < 10 ? "0" + minutes : minutes;
@@ -73,6 +93,14 @@
 		document.title = "(" + text + ") work timer";
  	};
 
+ 	function updatePlayButtontext() {
+ 		if (timerRunning) {
+	 		$('#start').text('pause');
+	 	} else {
+	 		$('#start').text('start');
+	 	}
+ 	}
+
  	function flashTab() {
  		var on = false;
  		window.setInterval(function () { on = !on; flashTabText(on) }, 1000);
@@ -87,13 +115,17 @@
  	}
 
  	function stopTimer() {
+ 		timerRunning = false;
  		updateClockText("00:00");
+ 		secondsRemaining = 0;
  		if (interval) {
  			clearInterval(interval);
  		}
  	};
 
  	function resetTimer(duration, display) {
+ 		timerRunning = false;
+ 		secondsRemaining = 0;
  		clearInterval(interval);
  		updateClockText($('#minutes').val() + ":00");
  	};
